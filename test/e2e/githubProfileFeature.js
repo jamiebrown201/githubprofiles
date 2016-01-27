b84871317d0aa19e19be973a9dc130131339a2ed
Jamie Brown <jamiebrown201@hotmail.com>
2016-01-27 11:03:05 +0000

    protractor find profile test

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,6 +1,21 @@
 describe('Github Profile finder', function(){
-  it('has a title', function() {
+  var searchBox = element(by.model('searchCtrl.searchTerm'));
+  var searchButton = element(by.className('btn'));
+
+  beforeEach(function() {
     browser.get('http://localhost:8080');
+  });
+
+  it('has a title', function() {
     expect(browser.getTitle()).toEqual('Github user search');
+  });
+
+  it('has a title', function() {
+
+    searchBox.sendKeys('jamiebrown201');
+    searchButton.click();
+
+    expect(element(by.binding('user.login')).getText())
+      .toEqual('jamiebrown201');
   });
 });
